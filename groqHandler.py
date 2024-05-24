4f05f3508a41a4a67703f4dc715c92d47b96d31b
BenLitzinger <ben.litzinger@whu.edu>
2024-05-24 15:08:37 +0200

    der groqqer ist bereit

--- groqHandler.py
+++ groqHandler.py
@@ -1,27 +1,39 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_groq import ChatGroq
+from groq import Groq
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-def batch():
-    chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
+#groq models: https://console.groq.com/docs/models
+class GroqModel:
+    def __init__(self, groqmodel, stream):
+        self.groqModel = groqmodel
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        self.stream = stream
 
-    system = "You are a helpful assistant."
-    human = "{text}"
-    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+    # Streaming
+    def generateAnswer(self, transcript):
+        stream = self.client.chat.completions.create(
+            messages = transcript,
+            model = self.groqModel,
+            stop = None,
+            stream = self.stream
+        )
 
-    chain = prompt | chat
-    print(chain.invoke({"text": "Explain the importance of low latency LLMs."}))
+        if self.stream:
+            for chunk in stream:
+                print(chunk.choies[0].delta.content, end="")
+        print(stream)        
+        return stream.choices[0].message.content
+            
+"""
+    def batch(self, transcript):
+        chat = ChatGroq(temperature=0, model_name=self.groqModel, groq_api_key=self.GroqApiKey)
 
-# Streaming
-def streaming():
-    chat = ChatGroq(temperature=0, model_name="llama2-70b-4096",groq_api_key=os.getenv("GROQ_API_KEY"))
-    prompt = ChatPromptTemplate.from_messages([("human", "Write a very long poem about {topic}")])
-    chain = prompt | chat
-    for chunk in chain.stream({"topic": "The Moon"}):
-        print(chunk.content, end="", flush=True)
+        system = "You are a helpful assistant."
+        human = "{text}"
+        prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
-# batch()
-streaming()
+        chain = prompt | chat
+        print(chain.invoke({"text": "Explain the importance of low latency LLMs."}))
+"""
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ElevenLabsHandler import ElevenLabsUtils
 from OpenaiHandler import OpenAIModel
 from VoskHandler import VoskModel
+from groqHandler import GroqModel
 
 from FileHandler import FileHandler
 from ai_assistant import AI_Assistant
@@ -8,7 +9,7 @@ from ai_assistant import AI_Assistant
 if __name__ == "__main__":
     fileHandler = FileHandler()
     prompt = fileHandler.readText("prompts/pizzaPrompt.txt")
-    ai_assistant = AI_Assistant(VoskModel("vosk-model-small-de-0.15"), OpenAIModel(), ElevenLabsUtils(voice_id= "VC9NHIQryLjTvEtbF4kj", model_id="eleven_multilingual_v2"), prompt)
+    ai_assistant = AI_Assistant(VoskModel("vosk-model-small-de-0.15"), GroqModel(groqmodel="mixtral-8x7b-32768", stream = False), ElevenLabsUtils(voice_id= "VC9NHIQryLjTvEtbF4kj", model_id="eleven_multilingual_v2"), prompt)
 
     fullTranscript = ai_assistant.start_conversation()
     fileHandler.writeText("transcripts/full_transcript.txt", fullTranscript)
