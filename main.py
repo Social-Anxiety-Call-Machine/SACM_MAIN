eb74241919f8647dfd49da43f59f14c0179f76c6
NickB42069 <nick.bermbach@gmail.com>
2024-05-24 15:26:07 +0200

    fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ElevenLabsHandler import ElevenLabsHandler
 from ElevenLabsHandlerWOLib import ElevenLabsHandlerWOLib
-from OpenaiHandler import OpenAIModel
-from VoskHandler import VoskModel
+from OpenaiHandler import OpenAIHandler
+from VoskHandler import VoskHandler
 from groqHandler import GroqModel
 
 from FileHandler import FileHandler
@@ -9,14 +9,14 @@ from ai_assistant import AI_Assistant
 
 if __name__ == "__main__":
     fileHandler = FileHandler()
-    prompt = fileHandler.readText("prompts/pizzaPrompt.txt")
+    prompt = FileHandler.readText("prompts/pizzaPrompt.txt")
 
-    stt = VoskModel("vosk-model-small-de-0.15")
-    llm = OpenAIModel()
+    stt = OpenAIHandler("vosk-model-small-de-0.15")
+    llm = VoskHandler()
     #tts = ElevenLabsHandler(voice_id= "VC9NHIQryLjTvEtbF4kj", model_id="eleven_multilingual_v2")
     tts = ElevenLabsHandlerWOLib(voice_id= "VC9NHIQryLjTvEtbF4kj", model_id="eleven_multilingual_v2")
 
     ai_assistant = AI_Assistant(stt, llm, tts, prompt)
 
     fullTranscript = ai_assistant.start_conversation()
-    #fileHandler.writeFullTranscript("transcripts/full_transcript.txt", fullTranscript)
+    #fileHandler.writeFullTranscript("transcripts/full_transcript.txt", fullTranscript)git checkout your-branch
